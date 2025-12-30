c5532b57d46b085ed5132e4d0411634541770507
Tran9523 <anstpfm1104@gmail.com>
2025-12-30 17:41:48 +0900

    chap3_101 : linux training folder add

---
+++ skill_book/chap3_101/101_main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include "101.h"
+
+int main(void)
+{
+    int number;
+
+    printf("This program converts DEC to reversed BINARY.\n");
+    printf("Enter the number: ");
+    scanf("%d", &number);
+
+    print_reversed_binary(number);
+
+    return 0;
+}
